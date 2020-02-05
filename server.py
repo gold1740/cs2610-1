812a3c62ccaf5473baa9e9c96420ef2b1d09c3dd
Peter Allen <peterjeremy@cableone.net>
2020-02-05 10:19:20 -0700

    started server code, not even close to being done.

---
+++ server.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+from time import strftime
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        
+
+
+print("Starting Server... press ctrl-c to quit.")
+
+server = BaseHTTPRequestHandler
+
